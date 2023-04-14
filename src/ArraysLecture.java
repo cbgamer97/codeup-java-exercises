3fdc12c120d9556adb3250555e24fe88b412537d
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-04-14 10:58:29 -0500

    notes on java arrays

--- src/ArraysLecture.java
+++ src/ArraysLecture.java
@@ -3,6 +3,12 @@ import rpg.Monster;
 import java.util.Arrays;
 public class ArraysLecture {
 
+    public static Monster[] addMonster(Monster[] monstersArray, Monster newMonster){
+        Monster[] newMonsterArray = Arrays.copyOf(monstersArray, monstersArray.length + 1);
+        newMonsterArray[newMonsterArray.length - 1] = newMonster;
+        return newMonsterArray;
+    }
+
     public static void main(String[] args) {
         Monster orc = new Monster("Orc", 13, 15, 9);
         Monster ogre = new Monster("Ogre", 11, 59, 13);
@@ -10,17 +16,24 @@ public class ArraysLecture {
 
         Monster[] monsters = {orc, ogre, blueDragon};
 
+        Monster cloaker = new Monster("Cloaker", 14, 78, 10);
+
+        Monster[] newMonsterArray = ArraysLecture.addMonster(monsters, cloaker);
+
 //        for (Monster monster : monsters){
 //            System.out.println(monster.getName());
-//
 //        }
+
+        for (Monster monster : newMonsterArray){
+            System.out.println(monster.getName());
+        }
 
         int[] moreNumbers = {33, 52, 22, 8, 1};
 
-        Arrays.sort(moreNumbers);
-        for (int number : moreNumbers){
-            System.out.println(number);
-        }
+//        Arrays.sort(moreNumbers);
+//        for (int number : moreNumbers){
+//            System.out.println(number);
+//        }
 
 
     }
--- src/rpg/RPG.java
+++ src/rpg/RPG.java
@@ -9,18 +9,20 @@ public class RPG {
         fighter1.setName("Arata");
         fighter1.setHitPoints(17);
         fighter1.setMaxDamage(14);
-        fighter1.printStats();
+//        fighter1.printStats();
 
         Fighter fighter2 = new Fighter();
         fighter2.setName("Sozalix");
         fighter2.setHitPoints(25);
         fighter2.setMaxDamage(23);
-        fighter2.printStats();
+//        fighter2.printStats();
 
         Fighter fighter3 = new Fighter("Shrek", 95, 45);
-        fighter3.printStats();
+//        fighter3.printStats();
 
         int d20Roll = D20.rollD20();
-        System.out.println(d20Roll);
+//        System.out.println(d20Roll);
+
+
     }
 }
